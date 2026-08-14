8dfd305068415c5b459b375e01d407f033e05bd4
XHooD <eugene22030122@gmail.com>
2026-08-14 17:20:34 +0300

    Rebuild content to Manual QA only

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
   spaceBetween: 20,
@@ -9,15 +9,15 @@ const swiper = new Swiper(".swiper", {
 
   // If we need pagination
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
     dynamicBullets: true,
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 
   zoom: {
